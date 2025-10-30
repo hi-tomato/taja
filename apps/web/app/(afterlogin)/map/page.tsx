971023fc8d284868710d6cec33fee6aa528353e5
이승준 <tmdwns092727@nate.com>
2025-10-30 22:52:35 +0900

    feat: Map 컴포넌트 추가

--- apps/web/app/(afterlogin)/map/page.tsx
+++ apps/web/app/(afterlogin)/map/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-import { API_KEY } from "@/constants/apikey";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import MapComponent from "@/components/map/MapComponent";
 
 export default function MapScreenPage() {
-  return (
-    <APIProvider apiKey={API_KEY.GOOGLE_MAPS}>
-      <Map
-        style={{ width: "100vw", height: "100vh" }}
-        defaultCenter={{ lat: 22.54992, lng: 0 }}
-        defaultZoom={3}
-        gestureHandling="greedy"
-        disableDefaultUI
-      />
-    </APIProvider>
-  );
+  return <MapComponent />;
 }
